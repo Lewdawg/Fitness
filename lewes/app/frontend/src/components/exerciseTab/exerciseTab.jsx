02d2456ae38b8c9d7a94ab0ef6ccc496d53adf17
Lewdawg <Leweswalton@gmail.com>
2021-04-30 14:45:37 +0200

    exerciseTab rework

---
+++ lewes/app/frontend/src/components/exerciseTab/exerciseTab.jsx
@@ -0,0 +1,26 @@
+import './exerciseTab.css';
+
+function ExerciseTab() {
+
+
+
+    return (
+
+        <>
+
+            <div className="theExerciseTab" style={{ width: '30%', height: '30%' }}>
+
+                <h2>Exercise.title</h2>
+
+
+                <div className="theExerciseTabCard" style={{ width: '100%', height: '80%' }}>
+                    <h1>Hello</h1>
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default ExerciseTab
