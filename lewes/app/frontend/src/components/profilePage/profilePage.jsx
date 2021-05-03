7e8243ba6c55e658b590bcfcf81208de7d84d3a4
Lewdawg <Leweswalton@gmail.com>
2021-05-03 15:26:43 +0200

    profilePage

---
+++ lewes/app/frontend/src/components/profilePage/profilePage.jsx
@@ -0,0 +1,34 @@
+import './profilePage.css'
+import banana from './banana.jpg'
+
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
+
+function ProfilePage() {
+
+
+
+    return (
+
+        <>
+            <div id="userProfilePage" style={{ width: '95%', height: '90%' }}>
+
+                <div className='pooBear' style={{ width: '58%', height: '95%' }}>
+
+
+                </div>
+
+                <div className="piglet" style={{ width: '38%', height: '95%' }}>
+
+
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default ProfilePage
